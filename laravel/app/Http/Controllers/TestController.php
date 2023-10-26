e6ba3d26247f7551cfbb696b52570003d1ab9345
zsombi222 <arvaizsombor@gmail.com>
2023-10-26 23:21:29 +0200

    Test model, migration, factory, seeder and controller

---
+++ laravel/app/Http/Controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Test;
+
+class TestController extends Controller
+{
+    public function index($id){
+        $test = Test::find($id);
+        echo $test->text;
+    }
+}
