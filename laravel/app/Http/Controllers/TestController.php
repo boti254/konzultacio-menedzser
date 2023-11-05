ede8e57203b3a2803e1d3563f1a924f28c7c535f
zsombi222 <arvaizsombor@gmail.com>
2023-11-05 15:43:58 +0100

    json header for test route

--- laravel/app/Http/Controllers/TestController.php
+++ laravel/app/Http/Controllers/TestController.php
@@ -8,7 +8,6 @@ use App\Models\Test;
 class TestController extends Controller
 {
     public function index($id){
-        $test = Test::find($id);
-        echo json_encode($test);
+        return Test::find($id);  
     }
 }
