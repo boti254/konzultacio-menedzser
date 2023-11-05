828eb16dd789488e1c39907a2f1cf038760212eb
boti254 <keve.botond24@gmail.com>
2023-11-05 22:52:24 +0100

    add simple api call

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,11 +1,50 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-	return (
-		<div>
-			React forntend.
-		</div>
-	)
+interface TestInterface {
+  id: number;
+  text: string;
 }
 
-export default App
+function App() {
+  const { data, loading } = useApi("https://szoftarch.webgravir.hu/api/test/1");
+  if (loading) {
+    return <div>Loading...</div>;
+  } else {
+    return <div>{data?.text}</div>;
+  }
+}
+
+function useApi(initialUrl: string) {
+  const [data, setData] = useState<TestInterface>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchData = async (url: string) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const result: TestInterface = await response.json();
+
+      setData(result);
+    } catch {
+      setError(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(initialUrl);
+  }, [initialUrl]);
+
+  return { data, loading, error, fetchData };
+}
+
+export default App;
