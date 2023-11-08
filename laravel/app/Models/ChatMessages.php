02abec77241c28655b9cf416699d777a49932b35
zsombi222 <arvaizsombor@gmail.com>
2023-11-08 18:18:53 +0100

    Chatmessage meeting, todo users models and migrations created

---
+++ laravel/app/Models/ChatMessages.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChatMessages extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'message',
+    ];
+
+    public function fromUser(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function toUser(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+}
--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('neptun')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->boolval('student');
+            $table->boolval('teacher');
+            $table->boolval('admin');
         });
     }
 
