e2c49fefb09b706a9cb0a11ab6ca56d180603bc7
zsombi222 <arvaizsombor@gmail.com>
2023-11-15 22:56:22 +0100

    create UserController, create User API, and fix TodoController

---
+++ laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index() {
+        $user = User::find(Auth::id());
+        if ($user && $user->admin) {
+            return User::all();
+        }
+        return response()->json([
+            'message' => 'Unauthorised'
+        ], 403);
+    }
+
+    public function search($expr) {
+        $user = User::find(Auth::id());
+        if ($user && $user->admin) {
+            return User::where('name', 'LIKE', '%'.$expr.'%')->get();
+        }
+        return response()->json([
+            'message' => 'Unauthorised'
+        ], 403);
+    }
+
+    public function getById($id) {
+        $user = User::find(Auth::id());
+        if ($user && ($user->admin || $id == Auth::id())) {
+            $user_to_find = User::find($id);
+            if ($user_to_find) {
+                return $user_to_find;
+            }
+            return response()->json([
+                'message' => 'User not found'
+            ], 400);
+        }
+        return response()->json([
+            'message' => 'Unauthorised'
+        ], 403);
+    }
+
+    public function updateOwnProfile(Request $request) {
+        $user = User::find(Auth::id());
+        if ($user) {
+            $req_array = [
+                'name' => $user->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'neptun' => $user->neptun,
+                'student' => $user->student,
+                'teacher' => $user->teacher,
+                'admin' => $user->admin,
+            ];
+            $user->update($req_array);
+            return $user;
+        }
+        return response()->json([
+            'message' => 'Unauthorised'
+        ], 403);
+
+    }
+
+    public function store(Request $request) {
+        $user = User::find(Auth::id());
+        if ($user && $user->admin) {
+            $new_user = null;
+            $req_array = [
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'neptun' => $request->neptun,
+                'student' => $request->student,
+                'teacher' => $request->teacher,
+                'admin' => $request->admin,
+            ];
+
+            if ($request->id == 0) {
+                $new_user = User::create($req_array);
+            }
+            else {
+                if ($request->id != Auth::id()){
+                    $user_to_mod = User::find($request->id);
+                    if ($user_to_mod){
+                        $user_to_mod->update($req_array);
+                        $new_user = $user_to_mod;
+                    }
+                    else{
+                        return response()->json([
+                            'message' => 'User not found'
+                        ], 400);
+                    }
+                }
+                else {
+                    return response()->json([
+                        'message' => 'Unauthorised'
+                    ], 403);
+                }
+
+            }
+            return $new_user;
+        }
+        return response()->json([
+            'message' => 'Unauthorised'
+        ], 403);
+    }
+
+    public function delete($id) {
+        $user = User::find(Auth::id());
+        if ($user && $user->admin && $id != Auth::id()) {
+            $user_to_delete = User::find($id);
+            if ($user_to_delete) {
+                $user_to_delete->delete();
+                return response()->json([
+                    'message' => 'User deleted'
+                ], 200);
+            }
+            return response()->json([
+                'message' => 'User not found'
+            ], 400);
+        }
+        return response()->json([
+            'message' => 'Unauthorised'
+        ], 403);
+    }
+}
