876b1458549508fab57067bbde26c6b3dbe2b273
boti254 <keve.botond24@gmail.com>
2023-11-15 18:30:10 +0100

    create UserEditPage layout

--- react/src/pages/UserEditPage/UserEditPage.tsx
+++ react/src/pages/UserEditPage/UserEditPage.tsx
@@ -1,7 +1,28 @@
+import "./UserEditPage.css";
 function UserEditPage() {
   return (
-    <div>
-      <h1>UserEditPage Works</h1>
+    <div className="user-edit-page-container">
+      <div className="user-input-wrapper">
+        <label htmlFor="userName">Név</label>
+        <input type="text" id="userName" placeholder="Nev" />
+      </div>
+      <div className="user-input-wrapper">
+        <label htmlFor="userEmail">Email</label>
+        <input type="email" id="userEmail" placeholder="Email" />
+      </div>
+      <div className="user-input-wrapper">
+        <label htmlFor="userNeptun">Neptun</label>
+        <input type="text" id="userNeptun" placeholder="Neptun" />
+      </div>
+      <div className="user-input-wrapper">
+        <label>Jogosultság</label>
+        <div className="user-role-container">
+          <button className="user-role">Hallgató</button>
+          <button className="user-role">Konzulens</button>
+          <button className="user-role">Admin</button>
+        </div>
+      </div>
+      <button className="save-btn">Mentés</button>
     </div>
   );
 }
