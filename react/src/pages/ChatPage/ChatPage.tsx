316386a574250bdf6895a09df134aa9652328fbf
boti254 <keve.botond24@gmail.com>
2023-11-16 22:26:25 +0100

    fix navigation error, build project

--- react/src/pages/ChatPage/ChatPage.tsx
+++ react/src/pages/ChatPage/ChatPage.tsx
@@ -1,69 +1,18 @@
-import { useEffect, useState } from 'react';
-import './ChatPage.css';
+import { useState } from "react";
+import "./ChatPage.css";
 
 function ChatPage() {
-  const [messages, setMessages] = useState<any[]>([]);
-  const [newMessage, setNewMessage] = useState<string>('');
-
-  // Assume your backend endpoint for fetching messages is '/api/messages'
-  const fetchMessageHistory = async () => {
-    try {
-      const response = await fetch('/api/messages');
-      if (response.ok) {
-        const messageHistory = await response.json();
-        setMessages(messageHistory);
-      } else {
-        console.error('Failed to fetch messages');
-      }
-    } catch (error) {
-      console.error('Error fetching messages:', error);
-    }
-  };
-
-  useEffect(() => {
-    // Fetch message history when the component mounts
-    fetchMessageHistory();
-  }, []);
-
-  const handleSendMessage = async () => {
-    if (newMessage.trim() !== '') {
-      const newMessageObj = {
-        content: newMessage,
-        sender: userType,
-        timestamp: new Date().toLocaleTimeString(),
-      };
-
-      // Simulate sending the new message to the backend
-      try {
-        const response = await fetch('/api/messages', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(newMessageObj),
-        });
-
-        if (response.ok) {
-          setMessages([...messages, newMessageObj]);
-          setNewMessage('');
-        } else {
-          console.error('Failed to send message');
-        }
-      } catch (error) {
-        console.error('Error sending message:', error);
-      }
-    }
-  };
+  const [newMessage, setNewMessage] = useState<string>("");
 
   return (
     <div className="chat-container">
       <div className="chat-messages">
-        {messages.map((message, index) => (
+        {/* {messages.map((message, index) => (
           <div key={index} className={`message ${message.sender}`}>
             <span className="timestamp">{message.timestamp}</span>
             <p>{message.content}</p>
           </div>
-        ))}
+        ))} */}
       </div>
       <div className="chat-input">
         <input
@@ -72,7 +21,13 @@ function ChatPage() {
           value={newMessage}
           onChange={(e) => setNewMessage(e.target.value)}
         />
-        <button onClick={handleSendMessage}>Send</button>
+        <button
+          onClick={() => {
+            return 0;
+          }}
+        >
+          Send
+        </button>
       </div>
     </div>
   );
--- react/src/pages/MenuPage/MenuPage.tsx
+++ react/src/pages/MenuPage/MenuPage.tsx
@@ -1,77 +1,109 @@
-import "./MenuPage.css"
-import { useNavigate } from 'react-router-dom';
+import "./MenuPage.css";
+import { useNavigate } from "react-router-dom";
 
 function MenuPage() {
   const navigate = useNavigate();
 
   const handleTodoStudentClick = () => {
-    navigate('todo-student');
-  }
+    navigate("/todo-student");
+  };
 
   const handleTodoTeacherClick = () => {
-    navigate('todo-teacher');
-   }
+    navigate("/todo-teacher");
+  };
 
   const handleChatContactsClick = () => {
-    navigate('chat-contacts');
-  }
+    navigate("/chat-contacts");
+  };
 
   const handleChatClick = () => {
-    navigate('chat');
-  }
+    navigate("/chat");
+  };
 
   const handleFhClick = () => {
-    navigate('users');
-  }
+    navigate("/users");
+  };
 
   const handleSaClick = () => {
-    navigate('user-edit');
-  }
+    navigate("/user-edit");
+  };
 
   const handleIszClick = () => {
-    navigate('appointment-edit-list');
-  }
+    navigate("/appointment-edit-list");
+  };
 
   const handleIjClick = () => {
-    navigate('appointment-list');
-  }
+    navigate("/appointment-list");
+  };
 
   const handleAppointmentStudentEditClick = () => {
-    navigate('appointment-student-edit');
-  }
+    navigate("/appointment-student-edit");
+  };
 
   const handleAppointmentTeacherEditClick = () => {
-    navigate('appointment-teacher-edit');
-  }
+    navigate("/appointment-teacher-edit");
+  };
 
   const handleListStudentClick = () => {
-    navigate('list-student');
-  }
+    navigate("/list-student");
+  };
 
   const handleListTeacherClick = () => {
-    navigate('list-teacher');
-  }
+    navigate("/list-teacher");
+  };
 
   const handleLogoutClick = () => {
     //TODO
-    navigate('/');
-  }
+    navigate("/");
+  };
 
   return (
     <div className="menu-container">
-      <button className="menu-button" onClick={handleTodoStudentClick}>TODO-Student</button>
-      <button className="menu-button" onClick={handleTodoTeacherClick}>TODO-Teacher</button>
-      <button className="menu-button" onClick={handleChatContactsClick}>ChatContacts</button>
-      <button className="menu-button" onClick={handleChatClick}>Chat</button>
-      <button className="menu-button" onClick={handleFhClick}>Felhasználók</button>
-      <button className="menu-button" onClick={handleSaClick}>Saját adatok</button>
-      <button className="menu-button" onClick={handleIszClick}>Időpont szerkesztése</button>
-      <button className="menu-button" onClick={handleIjClick}>Időpontra jelentkezés</button>
-      <button className="menu-button" onClick={handleAppointmentStudentEditClick}>AppointmentStudentEdit</button>
-      <button className="menu-button" onClick={handleAppointmentTeacherEditClick}>AppointmentTeacherEdit</button>
-      <button className="menu-button" onClick={handleListStudentClick}>ListStudent</button>
-      <button className="menu-button" onClick={handleListTeacherClick}>ListTeacher</button>
-      <button className="menu-logout-button" onClick={handleLogoutClick}>Kijelentkezés</button>
+      <button className="menu-button" onClick={handleTodoStudentClick}>
+        TODO-Student
+      </button>
+      <button className="menu-button" onClick={handleTodoTeacherClick}>
+        TODO-Teacher
+      </button>
+      <button className="menu-button" onClick={handleChatContactsClick}>
+        ChatContacts
+      </button>
+      <button className="menu-button" onClick={handleChatClick}>
+        Chat
+      </button>
+      <button className="menu-button" onClick={handleFhClick}>
+        Felhasználók
+      </button>
+      <button className="menu-button" onClick={handleSaClick}>
+        Saját adatok
+      </button>
+      <button className="menu-button" onClick={handleIszClick}>
+        Időpont szerkesztése
+      </button>
+      <button className="menu-button" onClick={handleIjClick}>
+        Időpontra jelentkezés
+      </button>
+      <button
+        className="menu-button"
+        onClick={handleAppointmentStudentEditClick}
+      >
+        AppointmentStudentEdit
+      </button>
+      <button
+        className="menu-button"
+        onClick={handleAppointmentTeacherEditClick}
+      >
+        AppointmentTeacherEdit
+      </button>
+      <button className="menu-button" onClick={handleListStudentClick}>
+        ListStudent
+      </button>
+      <button className="menu-button" onClick={handleListTeacherClick}>
+        ListTeacher
+      </button>
+      <button className="menu-logout-button" onClick={handleLogoutClick}>
+        Kijelentkezés
+      </button>
     </div>
   );
 }
