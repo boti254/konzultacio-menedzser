d4c7b2f5b2de651ed7841cd523a4e3554962ea01
Koplányi Dávid <55060571+KoplanyiDavid@users.noreply.github.com>
2023-11-16 12:08:00 +0100

    Some connection set between pages

--- react/src/pages/LoginPage/LoginPage.tsx
+++ react/src/pages/LoginPage/LoginPage.tsx
@@ -1,44 +1,46 @@
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import "./LoginPage.css"
 
 function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  const handleSubmit = (e: React.FormEvent) => {
-      e.preventDefault();
-      // Add your logic for handling the form submission (e.g., sending data to a server)
-      console.log('Email:', email);
-      console.log('Password:', password);
-  };
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        // Add your logic for handling the form submission (e.g., sending data to a server)
+        //sikeres bejelentkezes utan
+        navigate('menu');
+    };
 
-  return (
-      <div className="login-container">
-          <form className="login-form" onSubmit={handleSubmit}>
-              <label htmlFor="email">Email:</label>
-              <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-              />
+    return (
+        <div className="login-container">
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
 
-              <label htmlFor="password">Password:</label>
-              <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-              />
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
 
-              <button type="submit">Login</button>
-          </form>
-      </div>
-  );
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
 }
 
 export default LoginPage;
--- react/src/pages/MenuPage/MenuPage.tsx
+++ react/src/pages/MenuPage/MenuPage.tsx
@@ -1,15 +1,56 @@
 import "./MenuPage.css"
+import { useNavigate } from 'react-router-dom';
 
 function MenuPage() {
+  const navigate = useNavigate();
+
+  const handleTodoClick = () => {
+    //TODO
+    /* Későbbre
+    if (userType == "student") {
+      navigate('/todo-student');
+    }
+    else {
+      navigate('/todo-teacher');
+    }
+    */
+   navigate('todo-student');
+  }
+
+  const handleChatClick = () => {
+    navigate('chat-contacts');
+  }
+
+  const handleFhClick = () => {
+    navigate('users');
+  }
+
+  const handleSaClick = () => {
+    navigate('user-edit');
+  }
+
+  const handleIszClick = () => {
+    navigate('appointment-edit-list');
+  }
+
+  const handleIjClick = () => {
+    navigate('appointment-list');
+  }
+
+  const handleLogoutClick = () => {
+    //TODO
+    navigate('/');
+  }
+
   return (
     <div className="menu-container">
-      <button className="menu-button">TODO</button>
-      <button className="menu-button">Chat</button>
-      <button className="menu-button">Felhasználók</button>
-      <button className="menu-button">Saját adatok</button>
-      <button className="menu-button">Időpont szerkesztése</button>
-      <button className="menu-button">Időpontra jelentkezés</button>
-      <button className="menu-logout-button">Kijelentkezés</button>
+      <button className="menu-button" onClick={handleTodoClick}>TODO</button>
+      <button className="menu-button" onClick={handleChatClick}>Chat</button>
+      <button className="menu-button" onClick={handleFhClick}>Felhasználók</button>
+      <button className="menu-button" onClick={handleSaClick}>Saját adatok</button>
+      <button className="menu-button" onClick={handleIszClick}>Időpont szerkesztése</button>
+      <button className="menu-button" onClick={handleIjClick}>Időpontra jelentkezés</button>
+      <button className="menu-logout-button" onClick={handleLogoutClick}>Kijelentkezés</button>
     </div>
   );
 }
