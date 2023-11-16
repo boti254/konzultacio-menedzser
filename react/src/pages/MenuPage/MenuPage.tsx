8ccd409df98c84e8174d6dae346a8ada57fb10cd
Koplányi Dávid <55060571+KoplanyiDavid@users.noreply.github.com>
2023-11-16 20:51:28 +0100

    Add buttons for every page

--- react/src/pages/MenuPage/MenuPage.tsx
+++ react/src/pages/MenuPage/MenuPage.tsx
@@ -4,21 +4,20 @@ import { useNavigate } from 'react-router-dom';
 function MenuPage() {
   const navigate = useNavigate();
 
-  const handleTodoClick = () => {
-    //TODO
-    /* Későbbre
-    if (userType == "student") {
-      navigate('/todo-student');
-    }
-    else {
-      navigate('/todo-teacher');
-    }
-    */
-   navigate('todo-student');
+  const handleTodoStudentClick = () => {
+    navigate('todo-student');
+  }
+
+  const handleTodoTeacherClick = () => {
+    navigate('todo-teacher');
+   }
+
+  const handleChatContactsClick = () => {
+    navigate('chat-contacts');
   }
 
   const handleChatClick = () => {
-    navigate('chat-contacts');
+    navigate('chat');
   }
 
   const handleFhClick = () => {
@@ -37,6 +36,22 @@ function MenuPage() {
     navigate('appointment-list');
   }
 
+  const handleAppointmentStudentEditClick = () => {
+    navigate('appointment-student-edit');
+  }
+
+  const handleAppointmentTeacherEditClick = () => {
+    navigate('appointment-teacher-edit');
+  }
+
+  const handleListStudentClick = () => {
+    navigate('list-student');
+  }
+
+  const handleListTeacherClick = () => {
+    navigate('list-teacher');
+  }
+
   const handleLogoutClick = () => {
     //TODO
     navigate('/');
@@ -44,12 +59,18 @@ function MenuPage() {
 
   return (
     <div className="menu-container">
-      <button className="menu-button" onClick={handleTodoClick}>TODO</button>
+      <button className="menu-button" onClick={handleTodoStudentClick}>TODO-Student</button>
+      <button className="menu-button" onClick={handleTodoTeacherClick}>TODO-Teacher</button>
+      <button className="menu-button" onClick={handleChatContactsClick}>ChatContacts</button>
       <button className="menu-button" onClick={handleChatClick}>Chat</button>
       <button className="menu-button" onClick={handleFhClick}>Felhasználók</button>
       <button className="menu-button" onClick={handleSaClick}>Saját adatok</button>
       <button className="menu-button" onClick={handleIszClick}>Időpont szerkesztése</button>
       <button className="menu-button" onClick={handleIjClick}>Időpontra jelentkezés</button>
+      <button className="menu-button" onClick={handleAppointmentStudentEditClick}>AppointmentStudentEdit</button>
+      <button className="menu-button" onClick={handleAppointmentTeacherEditClick}>AppointmentTeacherEdit</button>
+      <button className="menu-button" onClick={handleListStudentClick}>ListStudent</button>
+      <button className="menu-button" onClick={handleListTeacherClick}>ListTeacher</button>
       <button className="menu-logout-button" onClick={handleLogoutClick}>Kijelentkezés</button>
     </div>
   );
