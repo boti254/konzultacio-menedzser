0c960245a2e1b65484369408e076511c29377e3a
boti254 <keve.botond24@gmail.com>
2023-11-16 22:29:54 +0100

    fix css override

--- react/src/pages/LoginPage/LoginPage.tsx
+++ react/src/pages/LoginPage/LoginPage.tsx
@@ -1,46 +1,54 @@
 import { useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import "./LoginPage.css"
+import { useNavigate } from "react-router-dom";
+import "./LoginPage.css";
 
 function LoginPage() {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        // Add your logic for handling the form submission (e.g., sending data to a server)
-        //sikeres bejelentkezes utan
-        navigate('menu');
-    };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Add your logic for handling the form submission (e.g., sending data to a server)
+    //sikeres bejelentkezes utan
+    navigate("menu");
+  };
 
-    return (
-        <div className="login-container">
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">Email:</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <label htmlFor="email" className="login-label">
+          Email:
+        </label>
+        <input
+          className="login-input"
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
 
-                <label htmlFor="password">Password:</label>
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
+        <label htmlFor="password" className="login-label">
+          Password:
+        </label>
+        <input
+          className="login-input"
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
 
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    );
+        <button className="login-button" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
