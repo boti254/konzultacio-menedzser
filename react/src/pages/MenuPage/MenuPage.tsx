4aa4fbff135c29cb5de79d1d5f44c62f92a146cf
Koplányi Dávid <55060571+KoplanyiDavid@users.noreply.github.com>
2023-11-16 09:39:58 +0100

    MenuPage created
    Basic design for menupage created. No logic so far

--- react/src/pages/MenuPage/MenuPage.tsx
+++ react/src/pages/MenuPage/MenuPage.tsx
@@ -1,7 +1,15 @@
+import "./MenuPage.css"
+
 function MenuPage() {
   return (
-    <div>
-      <h1>Menupage Works</h1>
+    <div className="menu-container">
+      <button className="menu-button">TODO</button>
+      <button className="menu-button">Chat</button>
+      <button className="menu-button">Felhasználók</button>
+      <button className="menu-button">Saját adatok</button>
+      <button className="menu-button">Időpont szerkesztése</button>
+      <button className="menu-button">Időpontra jelentkezés</button>
+      <button className="menu-logout-button">Kijelentkezés</button>
     </div>
   );
 }
