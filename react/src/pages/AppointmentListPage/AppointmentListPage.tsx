4a86b6d832da8d05d039ff66b978854ce9d5a28d
unknown <buranyidani@gmail.com>
2023-11-17 14:52:50 +0100

    Appointment oldalak elkezdése, listpage és editlistpage kész, teachereditpage félig kész

--- react/src/pages/AppointmentListPage/AppointmentListPage.tsx
+++ react/src/pages/AppointmentListPage/AppointmentListPage.tsx
@@ -1,7 +1,28 @@
+import BackButton from "../../components/BackButton/BackButton";
+import "./AppointmentListPage.css";
+
 function AppointmentListPage() {
+  const data = ["2023.10.10. 10:10", "2023.10.10. 11:11", "2023.10.10. 12:12"];
   return (
-    <div>
-      <h1>AppointmentListPage Works</h1>
+    <div className="appointment-listpage-container">
+      <BackButton linkTo={"/menu"} />
+      <div className="appointment-page-header">
+        <div className="header-title">
+          <h1>Konzultáció jelentkezés</h1>
+        </div>
+        <div className="appointment-select-container">
+          <select name="appointmentSelect" id="appointmentSelect">
+            <option value="ABC123">Pelda Bela</option>
+            <option value="XYZ987">Janos Fanos</option>
+          </select>
+        </div>
+      </div>
+      {data.map((appointment) => (
+        <div className="appointment-container" key={appointment}>
+          <div className="date-container">{appointment}</div>
+          <button className="icon-container km-icon-button-primary">+</button>
+        </div>
+      ))}
     </div>
   );
 }
