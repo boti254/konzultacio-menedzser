ddf43d0fded9c051c80c46b3702fd8267abd782f
boti254 <keve.botond24@gmail.com>
2023-11-18 12:58:59 +0100

    create users page api

--- react/src/pages/UsersPage/UsersPage.tsx
+++ react/src/pages/UsersPage/UsersPage.tsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
 import BackButton from "../../components/BackButton/BackButton";
 import "./UsersPage.css";
+import { useUser } from "../../hooks/useUsers";
 
 function UsersPage() {
-  const data = ["Peter", "Janos Janos"];
+  const { data, loading, fetchUsers } = useUser();
+  useEffect(() => {
+    fetchUsers("https://szoftarch.webgravir.hu/api/users");
+  }, []);
   return (
     <div className="users-page-container">
       <BackButton linkTo={"/menu"} />
@@ -18,13 +23,22 @@ function UsersPage() {
         />
         <button className="search-button km-button">Keresés</button>
       </div>
-      {data.map((user) => (
-        <div className="user-container" key={user}>
-          <div className="icon-container">ICON</div>
-          <div className="name-container">{user}</div>
-          <button className="icon-container km-icon-button-primary">M</button>
-        </div>
-      ))}
+      {loading
+        ? "Betöltés..."
+        : data?.map((user) => (
+            <div className="user-container" key={user.id}>
+              <div className="icon-container">ICON</div>
+              <div className="name-container">
+                {user.name} - {user.neptun}
+              </div>
+              <a
+                className="icon-container km-icon-button-primary"
+                href={`/user-edit/${user.id}`}
+              >
+                M
+              </a>
+            </div>
+          ))}
     </div>
   );
 }
