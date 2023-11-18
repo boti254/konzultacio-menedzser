f2f376640fcd60ebfe66f0e4ecef43b53b637fec
boti254 <keve.botond24@gmail.com>
2023-11-18 23:08:13 +0100

    fixes

--- react/src/pages/ListStudentPage/ListStudentPage.tsx
+++ react/src/pages/ListStudentPage/ListStudentPage.tsx
@@ -1,22 +1,23 @@
 import { useState } from "react";
-import "./ListStudentPage.css"
+import "./ListStudentPage.css";
 import BackButton from "../../components/BackButton/BackButton";
 
 const mockConsultants = [
-  { id: 1, name: 'John Doe' },
-  { id: 2, name: 'Jane Smith' },
-  { id: 3, name: 'Bob Johnson' },
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Smith" },
+  { id: 3, name: "Bob Johnson" },
 ];
 
 function ListStudentPage() {
-  const [searchTerm, setSearchTerm] = useState<string>('');
-  const [filteredConsultants, setFilteredConsultants] = useState(mockConsultants);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [filteredConsultants, setFilteredConsultants] =
+    useState(mockConsultants);
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const term = e.target.value.toLowerCase();
     setSearchTerm(term);
 
-    const filtered = mockConsultants.filter(consultant =>
+    const filtered = mockConsultants.filter((consultant) =>
       consultant.name.toLowerCase().includes(term)
     );
     setFilteredConsultants(filtered);
@@ -25,12 +26,11 @@ function ListStudentPage() {
   return (
     <div className="consultant-search-container">
       <BackButton linkTo={"/menu"} />
-      <label className="km-label default-margin">
-        Konzulensek listája
-      </label>
+      <label className="km-label default-margin">Konzulensek listája</label>
       <div className="default-margin">
         <input
           type="text"
+          className="km-input"
           placeholder="Konzulens keresése"
           value={searchTerm}
           onChange={handleSearch}
@@ -39,8 +39,11 @@ function ListStudentPage() {
       <div className="consultant-list">
         <h2>Konzulensek</h2>
         <ul>
-          {filteredConsultants.map(consultant => (
-            <li key={consultant.id}>{consultant.name}</li>
+          {filteredConsultants.map((consultant) => (
+            <li key={consultant.id}>
+              <span>{consultant.name}</span>
+              <button className="km-icon-button-primary">J</button>
+            </li>
           ))}
         </ul>
       </div>
--- react/src/pages/ListTeacherPage/ListTeacherPage.tsx
+++ react/src/pages/ListTeacherPage/ListTeacherPage.tsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
-import "./ListTeacherPage.css"
+import "./ListTeacherPage.css";
 import BackButton from "../../components/BackButton/BackButton";
 
 const mockStudents = [
-  { id: 1, name: 'John Doe' },
-  { id: 2, name: 'Jane Smith' },
-  { id: 3, name: 'Bob Johnson' },
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Smith" },
+  { id: 3, name: "Bob Johnson" },
 ];
 
 function ListTeacherPage() {
-  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [searchTerm, setSearchTerm] = useState<string>("");
   const [filteredStudents, setFilteredStudents] = useState(mockStudents);
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const term = e.target.value.toLowerCase();
     setSearchTerm(term);
 
-    const filtered = mockStudents.filter(student =>
+    const filtered = mockStudents.filter((student) =>
       student.name.toLowerCase().includes(term)
     );
     setFilteredStudents(filtered);
@@ -25,22 +25,25 @@ function ListTeacherPage() {
   return (
     <div className="student-search-container">
       <BackButton linkTo={"/menu"} />
-      <label className="km-label">
-        Hallgatók listája
-      </label>
+      <label className="km-label">Hallgatók listája</label>
       <div className="default-margin">
         <input
           type="text"
           placeholder="Hallgató keresése"
           value={searchTerm}
           onChange={handleSearch}
+          className="km-input"
         />
       </div>
       <div className="student-list">
         <h2>Hallgatók</h2>
         <ul>
-          {filteredStudents.map(student => (
-            <li key={student.id}>{student.name}</li>
+          {filteredStudents.map((student) => (
+            <li key={student.id}>
+              <span>{student.name}</span>
+              <button className="km-icon-button-primary">+</button>
+              <button className="km-icon-button-error">-</button>
+            </li>
           ))}
         </ul>
       </div>
