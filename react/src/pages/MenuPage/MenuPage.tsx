473b8702d98a46da5ed382a252e8bb8a6abcdb0f
boti254 <keve.botond24@gmail.com>
2023-11-20 22:51:31 +0100

    fix menupage

--- react/src/pages/MenuPage/MenuPage.tsx
+++ react/src/pages/MenuPage/MenuPage.tsx
@@ -1,35 +1,70 @@
+import { useEffect } from "react";
+import { useUser } from "../../hooks/useUser";
 import "./MenuPage.css";
-import { useParams } from "react-router-dom";
+//import { useParams } from "react-router-dom";
 
 function MenuPage() {
-  const { name } = useParams();
+  //const { name } = useParams();
+  const { fetchUser, user } = useUser();
+
+  useEffect(() => {
+    fetchUser(`https://szoftarch.webgravir.hu/api/users/me`);
+  }, []);
   return (
     <div className="menu-container">
-      {name ? <label className="km-label">{name}</label> : ""}
-      <a className="km-button" href="/todo-student">
-        TODO-Hallgató
-      </a>
-      <a className="km-button" href="/todo-teacher">
-        TODO-Konzulens
-      </a>
+      <label className="km-label">{user?.name}</label>
+      {user?.student ? (
+        <a className="km-button" href="/todo-student">
+          Feladatok
+        </a>
+      ) : (
+        ""
+      )}
+      {user?.teacher ? (
+        <a className="km-button" href="/todo-teacher">
+          Feladatok kiosztása
+        </a>
+      ) : (
+        ""
+      )}
       <a className="km-button" href="/chat-contacts">
         Chat Kontaktok
       </a>
-      <a className="km-button" href="/users">
-        Felhasználók
-      </a>
-      <a className="km-button" href="/appointment-edit-list">
-        Időpont szerkesztése
-      </a>
-      <a className="km-button" href="/appointment-list">
-        Időpontra jelentkezés
-      </a>
-      <a className="km-button" href="/list-student">
-        Hallgató lista
-      </a>
-      <a className="km-button" href="/list-teacher">
-        Konzulens lista
-      </a>
+      {user?.admin ? (
+        <a className="km-button" href="/users">
+          Felhasználók
+        </a>
+      ) : (
+        ""
+      )}
+      {user?.teacher ? (
+        <a className="km-button" href="/appointment-edit-list">
+          Időpont szerkesztése
+        </a>
+      ) : (
+        ""
+      )}
+      {user?.student ? (
+        <a className="km-button" href="/appointment-list">
+          Időpontra jelentkezés
+        </a>
+      ) : (
+        ""
+      )}
+      {user?.teacher ? (
+        <a className="km-button" href="/list-student">
+          Hallgató lista
+        </a>
+      ) : (
+        ""
+      )}
+      {user?.student ? (
+        <a className="km-button" href="/list-teacher">
+          Konzulens lista
+        </a>
+      ) : (
+        ""
+      )}
       <a className="km-button-error" href="/">
         Kijelentkezés
       </a>
