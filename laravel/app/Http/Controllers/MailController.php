ff9ede5699e6f6ab064af4605a69aee905ae94be
zsombi222 <arvaizsombor@gmail.com>
2023-11-21 16:09:14 +0100

    test mail

--- laravel/app/Http/Controllers/MailController.php
+++ laravel/app/Http/Controllers/MailController.php
@@ -10,7 +10,7 @@ class MailController extends Controller
     public function send() {
         $data = array('name' => "Joi die");
         Mail::send(['text' => 'mail'], $data, function($message) {
-            $message->to('arvaizsombor@gmail.com')->subject('Laravel Simple Mail Testing');
+            $message->to('arvaizsombor@gmail.com')->subject('Emlékeztető');
             $message->from('info@szoftarch.webgravir.hu' , 'Konzultáció Menedzser');
         });
         echo "Great! Simple mail successfully send!";
